0039a9eb877f9075c8abc80fc908661986c4af6c
erick555555 <erickdesousasantos942@gmail.com>
2024-10-01 08:22:49 -0300

    .

--- python/caixa.py
+++ python/caixa.py
@@ -1,9 +1,9 @@
 valor_saque = int(input("informe o valolr do saque"))
 
-notas200 = (valor_saque /200)
+notas200 = valor_saque /200
 notas100 = (valor_saque%200) /100
-notas50 = (valor_saque%50) /50
-notas20 = valor_saque%50 /20
+notas50 = (valor_saque%100) /50
+notas20 = (valor_saque%50) /20
 notas10 = (valor_saque%20) /10
 notas5 = (valor_saque%10) /5
 
